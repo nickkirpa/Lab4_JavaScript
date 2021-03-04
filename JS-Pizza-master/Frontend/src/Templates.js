9f16e73987fb852b73fc3b86118454f04c90d430
nickkirpa <77619291+nickkirpa@users.noreply.github.com>
2021-03-04 12:54:12 +0200

    Update Templates.js

--- JS-Pizza-master/Frontend/src/Templates.js
+++ JS-Pizza-master/Frontend/src/Templates.js
@@ -1,6 +1,3 @@
-/**
- * Created by chaika on 02.02.16.
- */
 var fs = require('fs');
 var ejs = require('ejs');
 
@@ -8,3 +5,8 @@ var ejs = require('ejs');
 exports.PizzaMenu_OneItem = ejs.compile(fs.readFileSync('./Frontend/templates/PizzaMenu_OneItem.ejs', "utf8"));
 
 exports.PizzaCart_OneItem = ejs.compile(fs.readFileSync('./Frontend/templates/PizzaCart_OneItem.ejs', "utf8"));
+
+exports.header = ejs.compile(fs.readFileSync('./Frontend/templates/header.ejs', "utf8"));
+exports.footer = ejs.compile(fs.readFileSync('./Frontend/templates/footer.ejs', "utf8"));
+exports.cart = ejs.compile(fs.readFileSync('./Frontend/templates/cart.ejs', "utf8"));
+exports.filters = ejs.compile(fs.readFileSync('./Frontend/templates/filters.ejs', "utf8"));
