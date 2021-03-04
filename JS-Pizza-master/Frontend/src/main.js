4f95ded1a1d5dd120249de96c69e9e76bc1f9f7e
nickkirpa <77619291+nickkirpa@users.noreply.github.com>
2021-03-04 12:53:51 +0200

    Update main.js

--- JS-Pizza-master/Frontend/src/main.js
+++ JS-Pizza-master/Frontend/src/main.js
@@ -3,13 +3,29 @@
  */
 
 $(function(){
-    //This code will execute when the page is ready
+
+    (function initTpls(tpls){
+        var Templates = require('./Templates');
+        for(var key in tpls){
+            var selector = tpls[key];
+            var $node = $(selector);
+            var _html = (Templates[key])({});
+            $node.html(_html);
+        }
+    })({
+        header: '#header',
+        footer: '#footer',
+        cart: '#cart',
+    });
+
     var PizzaMenu = require('./pizza/PizzaMenu');
     var PizzaCart = require('./pizza/PizzaCart');
-    var Pizza_List = require('./Pizza_List');
+    var OrderForm = require('./OrderForm');
 
     PizzaCart.initialiseCart();
     PizzaMenu.initialiseMenu();
+    OrderForm.init();
 
-
+    if (window.google) {
+        (require('./map')).initMap();}
 });
